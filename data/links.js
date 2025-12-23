b73abbc0bfc015ce94c0b4f729010ecb09259320
clubgestionlalogia <clubgestionlalogia@gmail.com>
2025-12-22 20:44:01 -0500

    202512220843pm

--- data/links.js
+++ data/links.js
@@ -2,7 +2,7 @@
 (function () {
   // Expone el objeto de enlaces para que sea accesible globalmente
   window.enlaces = {
-    "btn-barra": "barra/barra.html",
+    "btn-barra": "barra/ver-carta-completa.html",
     "btn-eventos": "eventos/eventos.html",
     "btn-momentos": "momentos/momentos.html",
     "btn-opinion": "https://www.google.com/maps/place/La+Logia/@6.2513021,-75.5873938,18z/data=!4m6!3m5!1s0x8e44293f52386fc1:0x64563794833ae5d0!8m2!3d6.2497539!4d-75.5885553!16s%2Fg%2F11scks2g5b?entry=ttu&g_ep=EgoyMDI1MTAwMS4wIKXMDSoASAFQAw%3D%3D",
